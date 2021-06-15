f0c37b026f0e4e23e26bb870bde5a39ea1827de1
Кира Кузнецова <kekuznetsova_1@miem.hse.ru>
2021-06-15 13:08:00 +0300

    modify conftest.py and test_items.py

--- conftest.py
+++ conftest.py
@@ -12,7 +12,7 @@ def browser(request):
 	language = request.config.getoption("--language")
 	options = Options()
 	options.add_experimental_option('prefs', {'intl.accept_languages': language})
-	browser = webdriver.Chrome('../chromedriver.exe', options=options)
+	browser = webdriver.Chrome(options=options)
 	yield browser
 	print("\nquit browser..")
 	browser.quit()
