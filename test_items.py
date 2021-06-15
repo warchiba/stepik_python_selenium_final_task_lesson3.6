06055ba88fd5b3b3eacdd518fcdb7453f2f1b057
warchiba <57146596+warchiba@users.noreply.github.com>
2021-06-15 12:58:17 +0300

    Add files via upload

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import pytest
+import time
+
+def test_button_add_to_cart(browser):
+	browser.implicitly_wait(10)
+	browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+	time.sleep(10)
+	elems = browser.find_elements_by_css_selector("button.btn-add-to-basket")
+	assert len(elems) != 0, \
+		"There are no button to cart on the page!"
+
